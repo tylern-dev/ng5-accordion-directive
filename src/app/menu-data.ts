d77a55584e5a82f1728fc0b929c115d72e4a7484
Tyler Negro <TylerNegro@Tylers-MBP.lan>
2018-03-16 22:17:51 -0600

    first init

---
+++ src/app/menu-data.ts
@@ -0,0 +1,9 @@
+import { Menu } from './menu';
+
+export const MENU: Menu[] = [
+  { id: 1, title: 'Define', children: ['TBD'], icon: 'fa fa-arrows-alt'},
+  { id: 2, title: 'Gather', children: ['Forms', 'Audit Results', 'Legacy Forms', 'Print Blank Form'], icon: 'fa fa-clipboard'},
+  { id: 3, title: 'See', children: ['TBD'], icon: 'fa fa-calendar'},
+  { id: 4, title: 'Act', children: ['TBD'], icon: 'fa fa-user'},
+  { id: 5, title: 'Admin', children: ['TBD'], icon: 'fa fa-cogs'}
+];
---
+++ src/app/menu.ts
@@ -0,0 +1,6 @@
+export class Menu {
+  id: number;
+  title: string;
+  children: Array<string>;
+  icon: string;
+}
